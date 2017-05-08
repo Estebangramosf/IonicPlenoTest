ef0a2d73c9aa2650c160482d3a5614a8d184a457
Esteban r <esteban.ramos.f@gmail.com>
2017-05-08 00:54:41 -0300

    update - making app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+/* Core Imports */
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { StatusBar, Splashscreen } from 'ionic-native';
 
+/* Replaced core import libs to the next refactor */
+//import { StatusBar } from '@ionic-native/status-bar';
+//import { SplashScreen } from '@ionic-native/splash-screen';
+
+/* Page Imports */
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+/* Angular mode to include all dependencies and components to use in the app */
 
 @NgModule({
   declarations: [
@@ -33,7 +41,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    Splashscreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
+/* Imports */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
 
+/* Initialize app */
 platformBrowserDynamic().bootstrapModule(AppModule);
